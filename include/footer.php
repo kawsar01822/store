29a429ab6f54d6bcd69eb87fe4410324b3892ea4
Kawsar Ahmed <kaws.med@gmail.com>
2023-01-01 00:37:25 +0600

    Create footer.php

---
+++ include/footer.php
@@ -0,0 +1,17 @@
+<div class="container">
+        <footer class="py-3 my-4">
+          <p class="text-center text-muted">© 2022 Company, Inc</p>
+        </footer>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+    <script src="resource/script.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#vendor').DataTable();
+        });
+    </script>
+</body>
+</html>
